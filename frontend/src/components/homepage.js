4064d63f897bc9575feae0677fea9fc54f3bb7f2
Ali Mustafa <24090078@lums.edu.pk>
2023-02-08 16:16:12 +0500

    Chaned link

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -83,7 +83,7 @@ function Homepage() {
                 {blog &&
                     blog.map((elem) => (
                         <Link
-                            to={`/api/${elem._id}`}
+                            to={`/${elem._id}`}
                             key={elem._id}
                             onClick={SingleBlog}
                         >
