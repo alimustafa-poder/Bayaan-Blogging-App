c4589bd9eaaf145e67cc8e88699e6e3e04a0c7a6
Ali Mustafa <24090078@lums.edu.pk>
2023-02-15 22:20:52 +0500

    Added Dark Mode

--- frontend/src/components/ErrorPage/404.js
+++ frontend/src/components/ErrorPage/404.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 export default function Page404() {
     //not found page
     return (
-        <div className="flex flex-col items-center justify-center h-screen gap-y-6">
+        <div className="flex flex-col items-center justify-center h-screen gap-y-6 dark:bg-slate-800">
             <h1 className="text-4xl sm:text-7xl text-red-400">
                 404: Page not found
             </h1>
             <Link to="/">
-                <button className="bg-gray-400 text-gray-800 px-4 py-2 rounded-md mt-4 hover:bg-gray-300">
+                <button className="bg-gray-400 text-gray-800 px-4 py-2 rounded-md mt-4 hover:bg-gray-300 dark:bg-slate-400 dark:hover:bg-slate-600">
                     Go back to homepage
                 </button>
             </Link>
